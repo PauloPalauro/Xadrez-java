aa502028e3547321463053885d10d49192f0f314
PauloPalauro <paulopalauro@gmail.com>
2024-02-01 01:00:36 -0300

    ChessException and ChessPosition

--- application/UI.java
+++ application/UI.java
@@ -6,8 +6,8 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces){
         System.out.println();
+        System.out.println();
         for(int i= 0; i<pieces.length; i++){
-            
             System.out.print((8 - i ) + "  ");
             for(int j=0; j<pieces.length; j++){
                 printPiece(pieces[i][j]);
